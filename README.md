b27c34b0accfa85eb196be681826c5fa9e69d9b7
fpaniaguajava <fernando.paniagua.java@gmail.com>
2026-03-05 11:10:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IFCD0014_2026_02_BIBLIOVIDEO
+Gestor de vídeos con Spring Boot + Postgre
---
+++ src/main/java/com/cursospring/IFCD0014_2026_02_BIBLIOVIDEO/Ifcd0014202602BibliovideoApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.IFCD0014_2026_02_BIBLIOVIDEO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ifcd0014202602BibliovideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ifcd0014202602BibliovideoApplication.class, args);
+	}
+
+}
